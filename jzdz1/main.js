5eae05703205d54629d753cac1dfed49a3efe83b
Solist-Alexander <fcco1233@gmail.com>
2023-05-03 17:38:30 +0300

    init commit

---
+++ jzdz1/main.js
@@ -0,0 +1,43 @@
+let string_a = 'hello';
+let string_b = 'owu';
+let string_c = 'ua';
+let num_a = 1;
+let num_b = 10;
+let num_c = -999;
+let num_d = 123;
+let num_e = 3.14;
+let num_f = 2.7;
+let num_g = 16;
+let bool_t = true;
+let bool_f = false;
+
+console.log(string_a + '.' + string_b+ '.' + string_c);
+console.log(num_a);
+console.log(num_b);
+console.log(num_c);
+console.log(num_d);
+console.log(num_e);
+console.log(num_f);
+console.log(num_g);
+console.log(bool_t);
+console.log(bool_f);
+
+let firstName ='alexander';
+let secondName = 'Mironchyk';
+let thirdName = 'Andreyovich';
+
+let user =` name ${firstName}, surname ${secondName}, thirdname ${thirdName}`
+console.log(user)
+
+let a = 100; let b = '100'; let c = true;
+
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+let prompt1 = prompt('напиши свое имя')
+let prompt2 = prompt('напиши свою фамилию')
+let prompt3 = prompt('напиши свое отчество')
+console.log(prompt1);
+console.log(prompt2);
+console.log(prompt3);
+console.log(prompt1 + ' ' + prompt2 + ' ' + prompt3);
